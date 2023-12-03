807e88efefdbaf404d4bd384964b95ca55533eb6
pestaton <parkerstaton1@gmail.com>
2023-12-03 17:37:36 -0500

    first commit

---
+++ Converter.py
@@ -0,0 +1,16 @@
+import os
+
+#file_folder = input("Input folder containing reports: ")
+
+path = os.getcwd()
+
+dir_list = os.listdir(path)
+
+print(dir_list)
+
+
+class converter:
+    def __init__(self):
+        self
+    
+
